be98b164143429693c48991b5a086a677b19588b
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-03 20:14:34 +0800

    used Provider tag for app

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <Provider>
+            <App />
+        </Provider>
     </StrictMode>
 );
