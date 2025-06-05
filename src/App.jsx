d96c106d3f965b7291ce42a6362f002274f992f0
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-05 16:38:57 +0800

    created nav bar component and routers in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-// src/App.jsx
+import NavBar from "./components/NavBar/NavBar";
+
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-    return <h1>Hello world!</h1>;
+    return (
+        <>
+            <NavBar />
+
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <main>
+                            <h1>Homepage</h1>
+                        </main>
+                    }
+                />
+                <Route path="/saved-flights" element={<h1>Saved Flights</h1>} />
+
+                <Route
+                    path="/saved-flights:bookingId"
+                    element={<h1>Saved Flights</h1>}
+                />
+            </Routes>
+        </>
+    );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -8,7 +9,9 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Provider>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </Provider>
     </StrictMode>
 );
