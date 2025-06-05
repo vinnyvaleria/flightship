855af79927fd51b87e22655cc70b1957b961e0bb
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-05 17:33:22 +0800

    completed inputs component for flight search

--- src/components/FlightSearch/FlightSearch.jsx
+++ src/components/FlightSearch/FlightSearch.jsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Input, Group, Stack } from "@chakra-ui/react";
+import { Button, Input, Stack, Field } from "@chakra-ui/react";
 
 const FlightSearch = () => {
     // state variable to store search term
     const [flightSearchTerm, setFlightSearchTerm] = useState([
-        { bookingId: "", apiKey: "" },
+        {
+            bookingId: "",
+            departureDate: "",
+            origin: "",
+            destination: "",
+            apiKey: "",
+        },
     ]);
 
     const handleInputChange = (event) => {
@@ -28,30 +34,91 @@ const FlightSearch = () => {
         <>
             <form onSubmit={handleSubmit}>
                 <Stack gap="4">
-                    <Input
-                        id="bookingId"
-                        name="bookingId"
-                        value={flightSearchTerm.bookingId}
-                        onChange={handleInputChange}
-                        variant="subtle"
-                        placeholder="Enter booking number"
-                    />
-                    <Input
-                        id="apiKey"
-                        name="apiKey"
-                        value={flightSearchTerm.apiKey}
-                        onChange={handleInputChange}
-                        variant="subtle"
-                        placeholder="Enter your api key"
-                    />
+                    <Field.Root>
+                        <Field.Label>Booking Number</Field.Label>
+                        <Input
+                            id="bookingId"
+                            name="bookingId"
+                            value={flightSearchTerm.bookingId}
+                            onChange={handleInputChange}
+                            variant="subtle"
+                            placeholder="Enter booking number"
+                            required
+                        />
+                        <Field.ErrorText>
+                            This field is required
+                        </Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root>
+                        <Field.Label>Departure date</Field.Label>
+                        <Input
+                            id="departureDate"
+                            name="departureDate"
+                            value={flightSearchTerm.departureDate}
+                            onChange={handleInputChange}
+                            variant="subtle"
+                            type="date"
+                            placeholder="Enter departure date"
+                            required
+                        />
+                        <Field.ErrorText>
+                            This field is required
+                        </Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root>
+                        <Field.Label>Origin City</Field.Label>
+                        <Input
+                            id="origin"
+                            name="origin"
+                            value={flightSearchTerm.origin}
+                            onChange={handleInputChange}
+                            variant="subtle"
+                            placeholder="Enter flight origin"
+                            required
+                        />
+                        <Field.ErrorText>
+                            This field is required
+                        </Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root>
+                        <Field.Label>Destination City</Field.Label>
+                        <Input
+                            id="destination"
+                            name="destination"
+                            value={flightSearchTerm.destination}
+                            onChange={handleInputChange}
+                            variant="subtle"
+                            placeholder="Enter flight destination"
+                            required
+                        />
+                        <Field.ErrorText>
+                            This field is required
+                        </Field.ErrorText>
+                    </Field.Root>
+                    <Field.Root>
+                        <Field.Label>API Key</Field.Label>
+                        <Input
+                            id="apiKey"
+                            name="apiKey"
+                            value={flightSearchTerm.apiKey}
+                            onChange={handleInputChange}
+                            variant="subtle"
+                            placeholder="Enter your api key"
+                            required
+                        />
+                        <Field.ErrorText>
+                            This field is required
+                        </Field.ErrorText>
+                    </Field.Root>
                     <Button
                         type="submit"
                         bg="bg.subtle"
                         variant="outline"
                         colorPalette="gray"
+                        required
                     >
                         Submit
-                    </Button>{" "}
+                    </Button>
                 </Stack>
             </form>
         </>
