699ad541e693e5b2872261bf535ea17426780228
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-08 14:14:49 +0800

    added flight service call function to pass into flight search

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-import { useState } from "react";
-import FlightsList from "./components/FlightsList/FlightsList";
+// src/App.jsx
+
 import NavBar from "./components/NavBar/NavBar";
 
-import { Route, Routes } from "react-router-dom";
+import FlightsList from "./components/FlightsList/FlightsList";
 import FlightsDetails from "./components/FlightsDetails/FlightDetails";
 import FlightSearch from "./components/FlightSearch/FlightSearch";
 
+import * as flightScheduledService from "./services/flightScheduleService";
+
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
     // state variable for list of flights saved based on search
-    const [flights, setFlights] = useState([]);
+    const [flightsData, setFlightsData] = useState([]);
+    const [displayedFlights, setDisplayedFlights] = useState([]);
+
+    // since API is fetch asynchronously, display Loading if needed
+    const [fLoading, setFLoading] = useState(true);
+
+    // function to fetch API data
+    const fetchFlightsData = async (formData) => {
+        try {
+            setFLoading(true);
+
+            const fData = await flightScheduledService.show(
+                formData.departureDate,
+                formData.departureIATA,
+                formData.arrivalIATA,
+                formData.apiKey
+            );
+
+            setFlightsData(fData);
+            setDisplayedFlights(fData);
+            setFLoading(false);
+
+            // return success for status check at flight search submission
+            return { success: true };
+        } catch (err) {
+            setFLoading(false);
+
+            // return error info
+            return {
+                success: false,
+                error: err.message || "Failed to fetch flights",
+            };
+        }
+    };
 
     return (
         <>
@@ -19,7 +57,7 @@ const App = () => {
                     path="/"
                     element={
                         <main>
-                            <FlightSearch />
+                            <FlightSearch fetch={fetchFlightsData} />
                         </main>
                     }
                 />
