4d41bd5d626e0efd2c67aa29794bfce2ba85c53f
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-09 18:37:12 +0800

    updated flight list to display flight cards based on mapping of response data

--- src/components/Flights/FlightsList.jsx
+++ src/components/Flights/FlightsList.jsx
@@ -1,9 +1,31 @@
+// src/components/Flights/FlightsList.jsx
+
+import { SimpleGrid } from "@chakra-ui/react";
 import FlightsDetails from "./FlightDetails";
 
-const FlightsList = ({ flights }) => {
-    return flights.map((flight) => {
-        <FlightsDetails flights={flight} />;
-    });
+import flights from "@/data/flights.json";
+
+const FlightsList = () => {
+    // available data from json file to use
+    const topFlights = flights.data.itineraries.topFlights;
+    const otherFlights = flights.data.itineraries.otherFlights;
+
+    // combine available flights data
+    const allFlights = [...topFlights, ...otherFlights];
+
+    return (
+        <>
+            <p>{allFlights.length} flights found.</p>
+            <SimpleGrid columns={{ base: 1, md: 2 }} gap="30px">
+                {allFlights.map((flight, id) => (
+                    <FlightsDetails
+                        key={flight.booking_token || id}
+                        flightData={flight}
+                    />
+                ))}
+            </SimpleGrid>
+        </>
+    );
 };
 
 export default FlightsList;
