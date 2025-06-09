54c871ceef6a1e0c2836396c09141b36e84365b1
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-10 02:25:04 +0800

    passing additional props for cards (loading, saved and duplicated)

--- src/components/Flights/FlightsList.jsx
+++ src/components/Flights/FlightsList.jsx
@@ -1,13 +1,13 @@
 // src/components/Flights/FlightsList.jsx
 
 import FlightsDetails from "./FlightCard";
-import useSaveFlight from "@/hooks/useSaveFlight";
+import useSavedFlights from "@/hooks/useSavedFlights";
 
-// import flights from "@/data/flights.json";
+import flights from "@/data/flights.json";
 
 import { SimpleGrid } from "@chakra-ui/react";
 
-const FlightsList = ({ flights, searchFormData }) => {
+const FlightsList = ({ searchFormData }) => {
     // initialize allFlights as an empty array by default
     let allFlights = [];
 
@@ -21,20 +21,29 @@ const FlightsList = ({ flights, searchFormData }) => {
     }
 
     // use the save flight hook to post data
-    const { saveFlight, savedFlights, savingFlights } =
-        useSaveFlight(searchFormData) || {};
+    const { saveFlight, savedFlights, savingFlights, isDuplicateBookingId } =
+        useSavedFlights(searchFormData) || {};
 
     return (
         <>
             <p>{allFlights?.length} flights found.</p>
             <SimpleGrid columns={{ base: 1, md: 2 }} gap={6}>
-                {allFlights.map((flight, id) => (
-                    <FlightsDetails
-                        key={flight.booking_token || id}
-                        flightData={flight}
-                        onSaveFlight={saveFlight}
-                    />
-                ))}
+                {allFlights.map((flight, id) => {
+                    const flightId = flight.booking_token || id;
+                    const isLoading = savingFlights.has(flightId);
+                    const isSaved = savedFlights.has(flightId);
+
+                    return (
+                        <FlightsDetails
+                            key={flightId}
+                            flightData={flight}
+                            onSaveFlight={saveFlight}
+                            isLoading={isLoading}
+                            isSaved={isSaved}
+                            isDuplicated={isDuplicateBookingId}
+                        />
+                    );
+                })}
             </SimpleGrid>
         </>
     );
