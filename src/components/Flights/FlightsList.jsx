cfb3a10d51bd68c738705518d1a1c31ec1b41b87
vinnyvaleria <vinny.valeria.i@gmail.com>
2025-06-10 01:56:04 +0800

    updated to test post function to saved flights table

--- src/components/Flights/FlightsList.jsx
+++ src/components/Flights/FlightsList.jsx
@@ -1,26 +1,38 @@
 // src/components/Flights/FlightsList.jsx
 
-import { SimpleGrid } from "@chakra-ui/react";
 import FlightsDetails from "./FlightCard";
+import useSaveFlight from "@/hooks/useSaveFlight";
 
-import flights from "@/data/flights.json";
+// import flights from "@/data/flights.json";
 
-const FlightsList = () => {
-    // available data from json file to use
-    const topFlights = flights.data.itineraries.topFlights;
-    const otherFlights = flights.data.itineraries.otherFlights;
+import { SimpleGrid } from "@chakra-ui/react";
 
-    // combine available flights data
-    const allFlights = [...topFlights, ...otherFlights];
+const FlightsList = ({ flights, searchFormData }) => {
+    // initialize allFlights as an empty array by default
+    let allFlights = [];
+
+    if (flights && flights.data?.itineraries) {
+        // available data from json file to use
+        const topFlights = flights.data.itineraries.topFlights;
+        const otherFlights = flights.data.itineraries.otherFlights;
+
+        // combine available flights data
+        allFlights = [...topFlights, ...otherFlights];
+    }
+
+    // use the save flight hook to post data
+    const { saveFlight, savedFlights, savingFlights } =
+        useSaveFlight(searchFormData) || {};
 
     return (
         <>
-            <p>{allFlights.length} flights found.</p>
+            <p>{allFlights?.length} flights found.</p>
             <SimpleGrid columns={{ base: 1, md: 2 }} gap={6}>
                 {allFlights.map((flight, id) => (
                     <FlightsDetails
                         key={flight.booking_token || id}
                         flightData={flight}
+                        onSaveFlight={saveFlight}
                     />
                 ))}
             </SimpleGrid>
